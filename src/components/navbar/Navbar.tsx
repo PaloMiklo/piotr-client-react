714ff5b1fce50e1adc4a50a5a4bd347f9f15a93c
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2023-08-09 14:59:23 +0200

    Navbar styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,29 +4,41 @@ import About from "../about/About";
 import Cart from "../cart/Cart";
 import Home from "../home/Home";
 import Products from "../products/Products";
+import './Navbar.scss';
 
 const Navbar = (): ReactElement<Element, string> => {
     return (
-        <BrowserRouter>
-            <div className="navigation-wrapper">
-                <nav>
-                    <nav>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/about">About</Link></li>
-                            <li><Link to="/products">Products</Link></li>
-                            <li><Link to="/cart">Cart</Link></li>
-                        </ul>
-                    </nav>
-                </nav>
+        <nav className="navbar navbar-expand-md navbar-light bg-white">
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarToggler"
+                aria-controls="navbarToggler"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarToggler"></div>
+            <BrowserRouter>
+                <div className="collapse navbar-collapse" id="navbarToggler">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item"><Link to="/">Home</Link></li>
+                        <li className="nav-item"><Link to="/about">About</Link></li>
+                        <li className="nav-item"><Link to="/products">Products</Link></li>
+                        <li className="nav-item"><Link to="/cart">Cart</Link></li>
+                    </ul>
+                </div>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/products" element={<Products />} />
                     <Route path="/cart" element={<Cart />} />
                 </Routes>
-            </div>
-        </BrowserRouter>
+            </BrowserRouter>
+        </nav>
     );
 }
 
