0d45dbdb2238c57db6ef08dcfb1829aaea45ca11
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2023-08-10 00:32:39 +0200

    List of products within tiles

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -2,29 +2,28 @@ import { ReactElement, useState, useEffect } from "react";
 import { IProduct } from "../../model/config";
 import { selectConfig } from "../../store/selector/selector";
 import { store } from "../../store/store";
-import Product from "./product/Product";
 import Tile from "./tile/Tile";
+import './Products.scss';
 
 const Products = (): ReactElement => {
     const [products, setProducts] = useState<IProduct[]>([]);
 
     useEffect(() => {
         const productsFromRedux = selectConfig(store.getState());
-        setProducts(productsFromRedux.mocks.products)
-    }, [products]);
+        setProducts(productsFromRedux.mocks.products);
+    }, []);
 
     return (
-        <div>
-            {
-                products.map((product: IProduct) => (
-                    <div key={product.id}>
-                        <Tile product={product} />
-                    </div>
-                ))
-            }
-
-        </div>
-    )
-}
+        <ul>
+            <li>
+                <div className="products">
+                    {products.map((product: IProduct) => (
+                        <Tile key={product.id} product={product} />
+                    ))}
+                </div>
+            </li>
+        </ul>
+    );
+};
 
 export default Products;
--- src/components/products/tile/Tile.tsx
+++ src/components/products/tile/Tile.tsx
@@ -4,7 +4,30 @@ import './Tile.scss';
 
 const Tile = ({ product }: TProductProps): ReactElement => {
     return (
-        <h1>{product.id}</h1>
+        <div className="col-sm-12 col-lg-3 product-tile">
+            <img
+                className="img-fluid mx-auto"
+                src={product.imagePath}
+                alt=""
+            />
+            <a href="#">
+                <div className="overlay">
+                    <div className="info">
+                        <span className="product-info-name">{product.name}</span>
+                        <span className="dash"></span>
+                        <p className="product-info-price">
+                            <span className="price">{product.price} EUR</span>
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <div className="buttons">
+                <button className="btn mw-100">
+                    <i className="fa fa-shopping-basket" aria-hidden="true"></i>
+                    <span> buy now</span>
+                </button>
+            </div>
+        </div>
     );
 }
 
