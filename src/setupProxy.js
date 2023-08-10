5cacd87b8c08e1bcfec8cca63a0229a5e9ee737f
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2023-08-10 09:42:00 +0200

    Proxy in package.json

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = (app) => {
-    app.use(createProxyMiddleware('/api', {
-      target: 'http://192.168.1.10:8080',
-      changeOrigin: true,
-  }));
-}
-
