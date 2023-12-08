f75e448818125b75bd514c9e653e27a6d6639541
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2023-12-08 16:51:27 +0100

    Configure proxy for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
     server: {
         open: true,
         port: 3000,
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            }
+        }
     },
     build: {
         rollupOptions: {
