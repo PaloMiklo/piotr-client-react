e95fe3add0c86b2c480bfcac980d57f6196d9eec
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2023-12-15 17:50:14 +0100

    Smaller brand and add animation on the brand logo

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -1,14 +1,30 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useState } from 'react';
 import LazyLoad from 'react-lazyload';
 import { Link } from 'react-router-dom';
+import { animated, useSpring } from 'react-spring';
+import './Brand.scss';
 
 const Brand: FC = (): ReactElement => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const props = useSpring({
+        from: { opacity: 0, transform: 'translateY(-50px)' },
+        to: { opacity: isHovered ? 0.8 : 1, transform: isHovered ? 'scale(1.1)' : 'scale(1)' },
+        config: { tension: 200, friction: 20 },
+    });
+
+    const handleMouseEnter = (): void => setIsHovered(true);
+    const handleMouseLeave = (): void => setIsHovered(false);
 
     return (
-        <div className="container">
+        <div className="container brand-wrapper">
             <Link to="/">
                 <LazyLoad once>
-                    <img className="img-fluid" src="/images/logo.png" loading="lazy" />
+                    <animated.div style={props}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        <img className="img-fluid" src="/images/logo.png" loading="lazy" />
+                    </animated.div>
                 </LazyLoad>
             </Link>
         </div>
